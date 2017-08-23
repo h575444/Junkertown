37de9c3c8e1048cde762c0f028616b0292d53612
Sondre <Sondre@158.37.235.175>
2017-08-23 14:12:11 +0200

    test message

--- EasyGraphicsTest/src/no/hvl/dat100/Grafikk.java
+++ EasyGraphicsTest/src/no/hvl/dat100/Grafikk.java
@@ -4,7 +4,7 @@ import easygraphics.*;
 public class Grafikk extends EasyGraphics {
 	public static void main(String[] args) {
 		launch(args);
-	}
+	} //random comment
 	public void run() {
 		makeWindow("Grafikk", 500, 350);
 		drawCircle(150, 130, 60);
